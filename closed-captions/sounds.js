63bdd2fd847dbcbfd8627afe2c9e47bfb655c7cf
RubySpee <94399952+RubySpee@users.noreply.github.com>
2023-05-23 10:42:04 +0200

    Add files via upload

--- closed-captions/sounds.js
+++ closed-captions/sounds.js
@@ -1,15 +1,23 @@
 // Hier kan je tijden toevoegen in secondes
-// Op die mommenten komt er een class op de body.
+// Op die momenten komt er een class op de body.
 sounds = [
-	0,
-	9,
-	12,
-	15,
+	0, //0
+	9, //1
+	11.9,
+	13, //3
+	15, //etc.
 	16.5,
-	24,
+	23,
+	23.5,
 	25.8,
-	31,
-	32,
-	34,
-	86
+	31.9,
+	33, //10 verhoor
+	37,
+	41,
+	46,
+	52, //14
+	57, //15
+	65, //16
+	69, //17
+	75.2 //18
 ];
